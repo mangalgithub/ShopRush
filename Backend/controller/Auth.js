b69a7eee9801e4260c54f5da4f38e26523dcff33
manish-bhukar <manish20223142@gmail.com>
2023-11-18 12:19:29 +0530

    jwt tokens created

--- Backend/controller/Auth.js
+++ Backend/controller/Auth.js
@@ -1,28 +1,36 @@
+const { sanitizeUser } = require("../Services/common");
 const { User } = require("../models/User");
+const crypto=require('crypto');
+const jwt=require('jsonwebtoken');
 exports.createUser = async (req, res) => {
-  const user = new User(req.body);
+ const SECRET_KEY='SECRET_KEY';
   try {
-    const response = await user.save();
-    res.status(201).json({id:response.id,role:response.role});
-  } catch (err) {
-    res.status(400).json(err);
+    
+    const salt = crypto.randomBytes(16);
+    crypto.pbkdf2(req.body.password, salt, 310000, 32, 'sha256',
+    async function(err, hashedPassword){
+      const user = new User({...req.body,password:hashedPassword,salt});
+      console.log(hashedPassword);
+      const doc=await user.save();
+      req.login(sanitizeUser(doc),(err)=>{
+        if(err){
+          res.status(400).json(err);
+        }
+        else{
+          const token=jwt.sign(sanitizeUser(doc),SECRET_KEY);
+          res.status(201).json(token);
+        }
+      })
+    
+    }
+  );}
+   catch (err) {    res.status(400).json(err);
   }
 };
 
 exports.loginUser=async(req,res)=>{
-    try {
-         const user =await User.findOne({ email: req.body.email }).exec(); 
-         console.log({user});
-       if(!user){
-        res.status(401).json({message:'no such user email'});
-       }
-       else if(user.password===req.body.password){
-            res.status(200).json({id:user.id,email:user.email, addresses:user.addresses,role:user.role});
-       }
-         else{
-            res.status(401).json({message:'wrong credentials'});
-         }
-    } catch (error) {
-        res.status(400).json(error);
-    }
+   res.json(req.user);
+}
+exports.checkUser=async(req,res)=>{
+  res.json(req.user);
 }
--- Backend/routes/Auth.js
+++ Backend/routes/Auth.js
@@ -1,6 +1,8 @@
 const express=require('express');
-const {createUser,loginUser} = require('../controller/Auth');
+const {createUser,loginUser,checkUser} = require('../controller/Auth');
 const router=express.Router();
-router.post('/signup',createUser).post('/login',loginUser);
+const passport=require('passport');
+router.post('/signup',createUser).post('/login',passport.authenticate('local'),loginUser)
+.get('/check',passport.authenticate('jwt'),checkUser);
 
 exports.router=router;
